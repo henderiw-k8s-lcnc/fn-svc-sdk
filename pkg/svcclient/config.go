ed3a1e8be73a7262e6722e356bd751328f75ea4a
Wim Henderickx <wim.henderickx@gmail.com>
2023-01-31 12:27:04 +0100

    added svcclient

--- pkg/svcclient/config.go
+++ pkg/svcclient/config.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package fnservice
+package svcclient
 
 type Config struct {
 	Address    string
